0d3b2c98a099359d194a04814a86a01e4fcb4b15
Haruki Ota <haruki@Moon.localdomain>
2019-02-27 05:04:26 +0900

    ボタンとカードにrippleを追加

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-import {MDCRipple} from '@material/ripple';
-import {MDCTextField} from '@material/textfield'
-import SweetScroll from "sweet-scroll"
+import { MDCRipple } from '@material/ripple';
+import { MDCTextField } from '@material/textfield';
+import SweetScroll from "sweet-scroll";
 
-var btns = document.querySelectorAll('.mdc-button');
-for (var i = 0, btn; btn = btns[i]; i++) {
-  MDCRipple.attachTo(btn);
-}
-
-var cards = document.querySelectorAll('.mdc-card__primary-action');
-for (var i = 0, card; card = cards[i]; i++) {
-  MDCRipple.attachTo(card);
-}
-
-var tfs = document.querySelectorAll('.mdc-text-field');
-for (var i = 0, tf; tf = tfs[i]; i++) {
-  MDCTextField.attachTo(tf);
-}
-
-const sweetScroll = new SweetScroll({
-  trigger: "a[href^='#']"
+let surfaces = document.querySelectorAll('.mdc-button, .book-card');
+surfaces.forEach(function (surface) {
+  new MDCRipple(surface);
 });
+
+// var tfs = document.querySelectorAll('.mdc-text-field');
+// for (var i = 0, tf; tf = tfs[i]; i++) {
+//   MDCTextField.attachTo(tf);
+// }
+
+const sweetScroll = new SweetScroll({ trigger: "a[href^='#']" });
