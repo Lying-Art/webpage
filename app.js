e1db24299ff41b30d975fbee84cca2c4e4f2c533
HRKo <ootaharuki99@gmail.com>
2019-02-28 04:50:25 +0900

    不要になったコメントアウト部分を削除

--- app.js
+++ app.js
@@ -18,21 +18,7 @@ helperTexts.forEach(function (helperText) {
   new MDCTextFieldHelperText(helperText);
 });
 
-// const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
-
-// var tfs = document.querySelectorAll('.mdc-text-field');
-// for (var i = 0, tf; tf = tfs[i]; i++) {
-//   MDCTextField.attachTo(tf);
-// }
-
 const sweetScroll = new SweetScroll({ trigger: "a[href^='#']" });
-
-// function onSubmit(token) {
-//   document.querySelector('.home_contact-form').submit();
-// }
-// function onSubmit(token) {
-//   document.getElementById("contact-form").submit();
-// }
 
 window.onload = function () {
   document.getElementById('home_contact-form-submit').onclick = function () {
@@ -47,25 +33,5 @@ window.onload = function () {
         clearInterval(loop);
       }
     }, 100)
-    // while (true) {
-    //   if (!!grecaptcha.getResponse()) {
-    //     document.getElementById("contact-form").submit();
-    //     return;
-    //   }
-    // }
   }
 }
-// function validate(event) {
-//   grecaptcha.execute();
-//   while (true) {
-//     if (!!grecaptcha.getResponse()) {
-//       document.getElementById("contact-form").submit();
-//       return;
-//     }
-//   }
-// }
-
-// function onload() {
-//   var element = document.getElementById('home_contact-form-submit');
-//   element.onclick = validate;
-// }
